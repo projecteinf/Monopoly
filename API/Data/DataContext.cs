ff07cfcbb90989da241916953c43819ac518d328
projecteinf <projecteinf@boscdelacoma.cat>
2023-09-30 06:23:03 -0600

    Afegit contexte DB. Inclusió taules Players i Streets

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace mba.Monopoly
+{
+    public class DataContext : DbContext {
+        
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Street> Streets { get; set; }
+
+    }
+}
