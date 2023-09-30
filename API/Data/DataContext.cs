a8321c021aa4192e543721790e0467cd42c2e5ac
projecteinf <projecteinf@boscdelacoma.cat>
2023-09-30 06:25:40 -0600

    Afegit Newtonsoft com a llibreria per a tractar les dades en format JSON

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -3,10 +3,8 @@ using Microsoft.EntityFrameworkCore;
 namespace mba.Monopoly
 {
     public class DataContext : DbContext {
-        
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Player> Players { get; set; }
         public DbSet<Street> Streets { get; set; }
-
     }
 }
