c68bc73112a2d82d47d81f97fc125c835f036514
projecteinf <projecteinf@boscdelacoma.cat>
2023-10-02 05:28:11 -0600

    Script per a crear els controladorssss

--- API/addController.sh
+++ API/addController.sh
@@ -1,1 +1,9 @@
-dotnet aspnet-codegenerator controller -name TeamController -async -api -m Team -dc DataContext -outDir Controllers
+# Exemple
+# dotnet aspnet-codegenerator controller -name TeamController -async -api -m Team -dc DataContext -outDir Controllers
+if [ $# -ne 1 ]
+  then
+    echo "Usage: ./addController.sh <ModelName>"
+    exit 1
+fi
+
+dotnet aspnet-codegenerator controller -name "$1Controller" -async -api -m $1 -dc DataContext -outDir Controllers
