b153e38bfadd11d88cb4a3dbb7925cdb31557763
projecteinf <projecteinf@boscdelacoma.cat>
2023-10-05 10:05:10 -0600

    Creació Microsoft SQL Server amb volum de disc

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-idContainer=$(docker container ls -a | cut -d " " -f1 | tail -n1)
-echo "Container" $idContainer
 
-if [ ! -z "$idContainer" ]
-    then
+cd Docker
+sudo docker-compose up -d
 
-        docker rm container $idContainer
-fi
+# idContainer=$(docker container ls -a | cut -d " " -f1 | tail -n1)
+# echo "Container" $idContainer
 
-docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Passw0rd!" \
-   -p 1433:1433 --name sql1 --hostname sql1 \
-   -d \
-   mcr.microsoft.com/mssql/server:2022-latest
+# if [ ! -z "$idContainer" ]
+#     then
 
-docker ps -a
+#         docker rm container $idContainer
+# fi
+
+# docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Passw0rd!" \
+#    -p 1433:1433 --name sql1 --hostname sql1 \
+#    -d \
+#    mcr.microsoft.com/mssql/server:2022-latest
+
+# docker ps -a
